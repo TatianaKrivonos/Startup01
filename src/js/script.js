57bc372edd32c271d158d45c2964d0d05815aa4a
TatianaKrivonos <konan_air.96@mail.ru>
2019-02-12 22:12:27 +0300

    убран placeholder на фокусе + добавлены активные элементы

--- src/js/script.js
+++ src/js/script.js
@@ -1,4 +1,6 @@
+
 $(document).ready(function() {
+  /*слайдер*/
   $('.j-team').slick({
   infinite: false,
   slidesToShow: 4,
@@ -24,5 +26,38 @@ $(document).ready(function() {
   $('.j-review').slick({
     arrows: false,
     dots: true
-  })
+  });
+
+  /*активные элементы*/
+  $(".tabs__link").click(function( event ) {
+    event.preventDefault();
+  });
+
+  $(".tabs__link--all").addClass("active");
+
+  $(".tabs__link").on("click", function(){
+    $(".tabs__link").removeClass("active");
+    $(this).addClass("active");
+  });
+
+  $("#slick-slide-control10").addClass("slick-dots__btn slick-dots__btn--first");
+  $("#slick-slide-control11").addClass("slick-dots__btn");
+  $("#slick-slide-control12").addClass("slick-dots__btn");
+
+  $(".slick-dots__btn--first").addClass("active");
+  $(".slick-dots__btn").on("click", function(){
+    $(".slick-dots__btn").removeClass("active");
+    $(this).addClass("active");
+  });
+
 });
+
+/*кнопка меню*/
+let toggle = document.getElementById('toggle');
+let menu = document.getElementById('menuList');
+
+  toggle.addEventListener('click', menuVisible);
+  function menuVisible(e) {
+    e.preventDefault();
+    menu.classList.toggle('main-nav__list--open');
+  };
